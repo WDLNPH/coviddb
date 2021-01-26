5cce997cc370f6f8d7a198e6e660efff9fac5045
belle-coder <63524016+belle-coder@users.noreply.github.com>
2021-01-26 17:47:41 -0500

    Update covidDB1.sql
    updated

--- covidDB1.sql
+++ covidDB1.sql
@@ -3,15 +3,15 @@ CREATE TABLE Person(
 serialID     	INT,
 firstName    	VARCHAR(30),
 lastName     	VARCHAR(30),
-dob    	  	DATE,
+dob	    	  	DATE,
 medicare     	INT,
-phone  	  	CHAR(16),
-address	  	VARCHAR(50),
-city   	  	VARCHAR(30),
+phone	  	  	CHAR(16),
+address		  	VARCHAR(50),
+city   	  		VARCHAR(30),
 province     	VARCHAR(15),
 postalCode   	CHAR(6),
 citizenship 	VARCHAR(20),
-email  	  	VARCHAR(30)
+email  	  		VARCHAR(30)
 );
 CREATE TABLE Parental(
 serialID        	INT,
@@ -28,4 +28,46 @@ serialID    	INT,
 groupZone   	VARCHAR(30),
 city        	VARCHAR(30),
 activity    	VARCHAR(30)
+);
+
+CREATE TABLE Diagnostic(
+	testID		INT,
+	serialID	INT, 
+	testDate	DATE, 
+	location	VARCHAR(50), 
+	employeeID	INT, 
+	result		BOOLEAN, 
+	resultDate	DATE
+);
+
+CREATE TABLE PublicHealthWorker(
+	employeeID		INT, 
+    post			VARCHAR(20), 
+    firstName		VARCHAR(30), 
+    lastName		VARCHAR(30), 
+    facilityID		INT, 
+    facilityName	VARCHAR(30), 
+    workShift		VARCHAR(30)
+);
+
+CREATE TABLE PHCEmployees(
+	employeeID		INT, 
+    post			VARCHAR(20), 
+    firstName		VARCHAR(30), 
+    lastName		VARCHAR(30), 
+    facilityID		INT, 
+    facilityName	VARCHAR(30), 
+    workShift		VARCHAR(30)
+);
+
+CREATE TABLE PHCTestedEmployees(
+	employeeID		INT, 
+    post			VARCHAR(20), 
+    firstName		VARCHAR(30), 
+    lastName		VARCHAR(30), 
+    facilityID		INT, 
+    facilityName	VARCHAR(30), 
+    workShift		VARCHAR(30),
+    testID			INT,
+    testResult		Boolean
 );
