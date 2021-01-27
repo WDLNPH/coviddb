47ccae91203a1709997c195b049205166186d0d0
belle-coder <63524016+belle-coder@users.noreply.github.com>
2021-01-27 10:01:34 -0500

    json

---
+++ webapp1/src/Components/SearchForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+
+function SearchForm(){
+    return(
+        <p>test</p>
+
+    );
+}
+export default SearchForm;
