9f846683e4cb8d75c464aa6dcd7a7c813a7767ef
belle-coder <63524016+belle-coder@users.noreply.github.com>
2021-01-31 21:21:06 -0500

    some correction

--- webapp1/src/components/DBparticipants.js
+++ webapp1/src/components/DBparticipants.js
@@ -5,40 +5,43 @@ import ReactDOM from 'react-dom';
 
 const axios = require('axios').default;
 
-export function DBparticipants() {
-    const [person, setPerson] = useState(null);
-    const [people, setPeople] = useState([]);
+const person= props =>{
+    return(
+        <Row>
+            <Col>{props.serialID}</Col>
+            <Col>{props.firstname}</Col>
+            <Col>{props.lastname}</Col>
+        </Row>
+    )
+};
 
+export function DBparticipants() {
     let count =0;
     axios.get('https://swapi.dev/api/people/')
         .then(function (response) {
             count = response.data.count;
             console.log("1. this is a count : ", count);
-
         })
         .catch(function (error) {console.log("ERROR");})
-        .then(function () {console.log(people);})
+        .then(function () {console.log("hi1");})
     let i=0;
-/*
-    function printOut(count) {
-        for(i=1; i<=count; i++){
-            let abc = document.getElementById("DBdiv1");
-            let rowid = "Row"+i;
-            console.log(rowid);
-            let rows = document.createElement('Row');
-            rows.id=rowid;
-            abc.appendChild(rows)
 
+    function printOut() {
+        for(i=1; i<=count; i++){
             let strAdd = 'https://swapi.dev/api/people/' + i;
             console.log("this is strAdd : ", strAdd);
-            let eleRow = <Row>
-                <Col>test1</Col>
-                <Col>test2</Col>
-            </Row>
-            ReactDOM.render(eleRow, document.getElementByID(rows));
+            axios.get(strAdd)
+                .then(function (response) {
+                    let obj1 = response.data;
+                    console.log(". this is a obj1: ", obj1);
+                    console.dir(obj1);
+                })
+                .catch(function (error) {console.log("ERROR");})
+                .then(function () {console.log("hi");})
+//            ReactDOM.render(eleRow, document.getElementByID(rows));
         }
     }
-*/
+
     return (
         <div id="DBdiv1">
             <p>  participants is exported </p>
@@ -46,21 +49,9 @@ export function DBparticipants() {
                 <column id="DBcol" >serial ID</column>
                 <column id="DBcol">First Name</column>
                 <column id="DBcol">Last Name</column>
-                <column id="DBcol">Date of Birth</column>
-                <column id="DBcol">Medicare ID</column>
-                <column id="DBcol">Phone</column>
-                <column id="DBcol">Address</column>
-                <column id="DBcol">City</column>
-                <column id="DBcol">Province</column>
-                <column id="DBcol">Postal Code</column>
-                <column id="DBcol">Citizenship</column>
-                <column id="DBcol">E-mail</column>
             </Row>
-            <Row >
-                {person ? (<div id="displayer">{person.name}</div>) : (<div ></div>)}
-                {people.map(function (people) {return (<div> <p>test</p>{people.name}</div>)})}
+            <Row>{printOut()}
             </Row>
-
         </div>
 
     );
