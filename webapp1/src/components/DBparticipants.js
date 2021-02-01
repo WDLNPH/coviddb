8a48e4156ef92f9ed228dde23cb0a666398fef1f
Benjamin Lofo Follo <benjamin.lofo@gmail.com>
2021-01-31 21:34:35 -0500

    changes and comments

--- webapp1/src/components/DBparticipants.js
+++ webapp1/src/components/DBparticipants.js
@@ -1,31 +1,46 @@
-import React, {useState} from 'react';
-import {div, Button, Row, Col} from "react-bootstrap";
+import React, {useState, useEffect} from 'react';
+import {div, Button, Row, Col, Table} from "react-bootstrap";
 import "../css/generalStyling.css";
 import ReactDOM from 'react-dom';
 
 const axios = require('axios').default;
 
-const person= props =>{
-    return(
+// Whenever you return react elements, the function must be capitalized
+const Person = props =>{
+    return (
         <Row>
-            <Col>{props.serialID}</Col>
-            <Col>{props.firstname}</Col>
-            <Col>{props.lastname}</Col>
+            <Col>{props.person.birth_year}</Col>
+            <Col>{props.person.name}</Col>
+            <Col>{props.person.gender}</Col>
         </Row>
     )
 };
 
 export function DBparticipants() {
-    let count =0;
-    axios.get('https://swapi.dev/api/people/')
-        .then(function (response) {
-            count = response.data.count;
-            console.log("1. this is a count : ", count);
-        })
-        .catch(function (error) {console.log("ERROR");})
-        .then(function () {console.log("hi1");})
-    let i=0;
+    let count = 0;
+    // If you remember, this is what I used last time to get the array in the Database Component
+    const [people, setPeople] = useState([])
 
+    // This is the key to everything, and we'll need to explain through vc how it works
+    // Basically, this makes it so when the component runs for the first time, it loads
+    // the API call to initialize your values. its a replacement for componentDidMount
+    // See it here: https://reactjs.org/docs/hooks-effect.html
+    useEffect(() => {
+        axios.get('https://swapi.dev/api/people/')
+            .then((response) => {
+                // Instead of doing the foreach, i add all of my results here
+                // I then run 1 call instead of 10, which is more efficient
+                setPeople(response.data.results);
+                // console.log("1. this is a count : ", count);
+            })
+            .catch(function (error) {console.log("ERROR");})
+            .then(function () {console.log("hi1");})
+    },[])
+
+    /**
+     *
+     * These are no longer needed, and you'll see by the way the state is used below
+    let i=0;
     function printOut() {
         for(i=1; i<=count; i++){
             let strAdd = 'https://swapi.dev/api/people/' + i;
@@ -41,17 +56,28 @@ export function DBparticipants() {
 //            ReactDOM.render(eleRow, document.getElementByID(rows));
         }
     }
+    */
+
 
     return (
         <div id="DBdiv1">
-            <p>  participants is exported </p>
+            <p>participants is exported </p>
+            <Table>
             <Row>
                 <column id="DBcol" >serial ID</column>
                 <column id="DBcol">First Name</column>
                 <column id="DBcol">Last Name</column>
             </Row>
-            <Row>{printOut()}
-            </Row>
+
+            {/*<Row>{printOut()}</Row>*/}
+
+
+            {/*
+                Alright, so using the state, i'm passing over each array object
+                to the component you've created. no need for the printOut! :)
+            */}
+            {people.map(person => <Person person={person}/>)}
+            </Table>
         </div>
 
     );
