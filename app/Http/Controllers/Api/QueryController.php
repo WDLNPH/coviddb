0c557736aa5fbd002930949aadb261a192734982
Benjamin Lofo Follo <benjamin.lofo@gmail.com>
2021-02-04 12:10:05 -0500

    create Queries in the back end

--- app/Http/Controllers/Api/QueryController.php
+++ app/Http/Controllers/Api/QueryController.php
@@ -13,6 +13,20 @@ class QueryController extends Controller
      * (first-name, last-name, date of birth, email, phone, city).
      */
     public function caseOne() {
+        $query = "SELECT
+                p.first_name,
+                p.last_name,
+                p.dob,
+                p.email,
+                p.phone,
+                p.city
+            FROM Person p
+            JOIN GroupZonePersonPivot gzp
+                ON p.person_id = gzp.person_id
+            JOIN GroupZone gz
+                ON gz.group_id = gzp.group_id
+            WHERE
+                gz.group_name = 'MontrealPrimaryGrade1_Group_1'";
         return $this->getDefaultResponse();
     }
 
@@ -21,6 +35,22 @@ class QueryController extends Controller
      * of all the people who tested positive for the COVID-19 on January 10 , 2021
      */
     public function caseTwo() {
+        $query = "SELECT
+                p.first_name,
+                p.last_name,
+                p.dob,
+                p.email,
+                p.phone,
+                p.city
+            FROM Person p
+            JOIN Patient pt
+                ON p.person_id = pt.person_id
+            JOIN Diagnostic d
+                ON pt.patient_id = d.patient_id
+            WHERE
+                d.result = TRUE
+                AND d.diagnostic_date BETWEEN '2021-01-10 00:00:00'
+                    AND '2021-01-10 23:59:59'";
         return $this->getDefaultResponse();
     }
 
@@ -30,6 +60,21 @@ class QueryController extends Controller
      * Include the history of the diagnosis if a person have been tested more than once).
      */
     public function caseThree() {
+        $query = "SELECT
+                p.first_name,
+                p.last_name,
+                p.dob,
+                p.email,
+                p.phone,
+                d.diagnostic_date, # TODO: Last Diagnostic Date
+                GROUP_CONCAT() # TODO: History of diagnosis
+            FROM Person p
+            JOIN Patient pt
+                ON p.person_id = pt.person_id
+            JOIN Diagnostic d
+                ON pt.patient_id = d.patient_id
+            WHERE
+                p.address = '95 Robert St.'";
         return $this->getDefaultResponse();
     }
 
@@ -38,6 +83,22 @@ class QueryController extends Controller
      * first- name, last-name, date of birth, email, phone).
      */
     public function caseFour() {
+        $query = "SELECT
+                p.first_name,
+                p.last_name,
+                p.dob,
+                p.email,
+                p.phone
+            FROM Person p
+            WHERE
+                p.address = (
+                    SELECT
+                       address
+                    FROM Person
+                        WHERE first_name = 'Roger'
+                        AND last_name = 'Macdonald'
+                )";
+
         return $this->getDefaultResponse();
     }
 
@@ -47,6 +108,27 @@ class QueryController extends Controller
      * of people in that specific GroupZone).
      */
     public function caseFive() {
+        $query = "SELECT
+                p.first_name,
+                p.last_name,
+                p.dob,
+                p.email,
+                p.phone,
+                p.city
+            FROM Person p
+            JOIN GroupZonePersonPivot gzp
+                ON p.person_id = gzp.person_id
+            WHERE
+                gzp.group_id IN (
+                    SELECT
+                        gzp.group_id
+                    FROM GroupZonePersonPivot gzp
+                    JOIN Person p
+                        ON p.person_id = gzp.person_id
+                    WHERE p.first_name = 'Roger'
+                    AND p.last_name = 'Macdonald'
+                )";
+
         return $this->getDefaultResponse();
     }
 
@@ -55,6 +137,19 @@ class QueryController extends Controller
      *  (first-name, last-name, date of birth, email, phone, city).
      */
     public function caseSix() {
+        $query = "SELECT
+                p.first_name,
+                p.last_name,
+                p.dob,
+                p.email,
+                p.phone,
+                p.city
+            FROM Person p
+            JOIN Employee e
+                ON p.person_id = e.person_id
+            JOIN Facility f
+                ON f.facility_id = e.facility_id
+            WHERE f.facility_name = 'Viau'";
         return $this->getDefaultResponse();
     }
 
