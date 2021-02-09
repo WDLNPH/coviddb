b73b6f7dcb7bad17eb49ca531cf0a511c90be3a2
AsAntoine <antoine.assal@mail.concordia.ca>
2021-02-08 21:08:20 -0500

    foreign keys

--- app/Console/Commands/test.php
+++ app/Console/Commands/test.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Faker;
 use Illuminate\Support\Facades\DB;
+use mysqli;
 
 class test extends Command
 {
@@ -39,14 +40,23 @@ class test extends Command
      */
     public function handle()
     {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "coviddb";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+
         $faker = Faker\Factory::create();
+
         $faker->addProvider(new Faker\Provider\en_CA\Address($faker));
         $faker->addProvider(new Faker\Provider\fr_CA\Person($faker));
         $faker->addProvider(new Faker\Provider\en_CA\Custom($faker));
         for ($i = 0; $i < 20; $i++) {
             DB::table("person")->insert([
                 // Person ID
-                "medicare" => $faker-> medicare,            
+                "medicare" => $faker->medicare,
                 "first_name" => $faker->firstName,
                 "last_name" => $faker->lastName,
                 "address" => $faker->streetAddress,
@@ -71,12 +81,37 @@ class test extends Command
                 "website"  => $faker->url,
 
             ]);
+            $person_id = array();
+            $select_person_id = "SELECT person_id FROM Person";
+            $result = $conn->query($select_person_id);
+            if ($result) {
+                while ($row = $result->fetch_assoc()) {
+                    array_push($person_id, $row["person_id"]);
+                }
+            } else {
+                echo "esti man jtanner";
+            }
+
+            $center_id = array();
+            $select_center_id = "SELECT health_center_id FROM PublicHealthCenter";
+            $result = $conn->query($select_center_id);
+            if ($result) {
+                while ($row = $result->fetch_assoc()) {
+                    array_push($center_id, $row["health_center_id"]);
+                }
+            } else {
+                echo "esti man jtanner";
+            }
+
+           
+
             DB::table("publichealthworker")->insert([
-                
-                //"health_worker_id" => $faker->relation('person', 'id'),
-                // `schedule` => dates of work?
-                "position" => $faker->firstNameMale, // this breaks
-           ]);
+                "health_center_id" => $faker->randomElement($center_id),
+                "person_id" => $faker->randomElement($person_id),
+                "schedule" => $faker->type,
+                "position" => $faker->position,
+            
+            ]);
         }
     }
 }
