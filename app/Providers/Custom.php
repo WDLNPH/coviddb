07ace4342bcca63ed115ff057a481b434a1e4878
Rami <sheepithere@hotmail.com>
2021-02-13 22:01:00 -0500

    Schedule for workers updated

--- app/Providers/Custom.php
+++ app/Providers/Custom.php
@@ -10,12 +10,22 @@ class Custom extends \Faker\Provider\DateTime
     protected static $letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     protected static $hcPosition = ['Janitor', 'Doctor', 'Nurse', 'Psycho', 'Intern'];
     protected static $hcType = ['Hospital', 'Clinic', 'Special Installment'];
-    protected static $schedule;
 
-    public static function schedule_builder()
+
+
+    //generates time 24hr within specified format
+    public static function schedule_timer()
     {
-        $schedule = Carbon::now()->format('H:00:00-H:00:00');
-        return static::toUpper($schedule);
+        $time = now()->setHour(rand(0,23));           
+        return $time->format('H:00') . '-' . $time->addHours(8)->format('H:00');
+    }
+
+    //builds fake weekly schedule
+    public static function schedule_builder()
+    {     
+        $sched = "Mon: " . self::schedule_timer() . ", Tue: " . self::schedule_timer() . ", Wed: " . self::schedule_timer() . ", Thu: " . self::schedule_timer() 
+        . ", Fri: " . self::schedule_timer() . ", Sat: " . self::schedule_timer() . ", Sun: " . self::schedule_timer();         
+        return $sched;
     }
 
     public static function randomMedicareLetter()
