52abe8e41a78c0bb4fe26c9082c64210df9e2556
Benjamin Lofo Follo <benjamin.lofo@gmail.com>
2021-02-14 07:05:36 -0500

    use table striped

--- webapp1/src/components/Table.js
+++ webapp1/src/components/Table.js
@@ -35,7 +35,7 @@ export default function Table({columns, data}) {
       - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
     */
     return (
-        <table {...getTableProps()}>
+        <table className="table table-striped" {...getTableProps()}>
             <thead>
             {headerGroups.map(headerGroup => (
                 <tr {...headerGroup.getHeaderGroupProps()}>
