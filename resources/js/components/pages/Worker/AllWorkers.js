a03677953cfdfd26a32016bb57791329fc3f4855
Belle-coder <ribelle.el.ayoubi@gmail.com>
2021-04-03 14:19:08 -0400

    worker form UI looks nice. we just need to configure all the rest

--- resources/js/components/pages/Worker/AllWorkers.js
+++ resources/js/components/pages/Worker/AllWorkers.js
@@ -71,79 +71,60 @@ export default function () {
 
             <div className="-mx-3 md:flex md:flex-col mb-2">
                 <div className="md:flex md:flex-row px-3 mb-6 md:mb-0">
-                    <div className="md:w-1/3 uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" >
-                        Time
+                    <div className="md:w-1/3 uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" >Schedule input</div>
+                </div>
+                <div className="-mx-3 md:flex mb-2">
+                    <div className="grid-cols-2">
+                        <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Monday</label>
+                            <input type="time"
+                                   className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                            <input type="time"
+                                   className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
-                    <div className="md:w-1/3 uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-                        Start<br/>
+                    <div className="grid-cols-2">
+                        <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Tuesday</label>
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
-                    <div className="md:w-1/3 uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-                        End
+                    <div className="grid-cols-2">
+                        <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Wednesday</label>
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                    </div>
+                    <div className="grid-cols-2">
+                        <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Thursday</label>
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                    </div>
+                    <div className="grid-cols-2">
+                        <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Friday</label>
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                    </div>
+                    <div className="grid-cols-2">
+                        <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Saturday</label>
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                    </div>
+                    <div className="grid-cols-2">
+                        <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Sunday</label>
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time"
+                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
                 </div>
-                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Monday
-                    </label>
-                    <input type="time"
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" />
-                </div>
-                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Tuesday
-                    </label>
-                    <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="text" placeholder="time"/>
-                </div>
-                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Wednesday
-                    </label>
-                    <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="text" placeholder="time"/>
-                </div>
-                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Thursday
-                    </label>
-                    <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="text" placeholder="time"/>
-                </div>
-                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Friday
-                    </label>
-                    <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="text" placeholder="time"/>
-                </div>
-                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Saturday
-                    </label>
-                    <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="text" placeholder="time"/>
-                </div>
-                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Sunday
-                    </label>
-                    <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="text" placeholder="time"/>
-                </div>
+
             </div>
             <div className="md:w-1/2 px-3 mb-6 md:mb-0">
                 <button type="submit" class="bg-transparent hover:bg-grey text-grey-dark font-semibold hover:text-white py-2 px-4 border border-grey hover:border-transparent rounded mr-2"> Submit </button>
