d7a63b243847d53f9d0ca3d0a0aa9c7ca7635907
Belle-coder <ribelle.el.ayoubi@gmail.com>
2021-04-04 09:55:49 -0400

    for workers cleaned element tags for all facilites, created form

--- resources/js/components/pages/Facility/AllFacilities.js
+++ resources/js/components/pages/Facility/AllFacilities.js
@@ -1,5 +1,102 @@
 import React from 'react';
+import {Field} from "formik";
 
-export default function () {
-    return <>Implement list here</>
+export default function ()   {
+
+function handleSubmit() {
+    alert('A form was submitted ' );
+}
+return  (       <form onSubmit={handleSubmit}>
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
+        <div className="-mx-3 md:flex mb-6">
+            <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                       htmlFor="grid-first-name">
+                    Name of Facility
+                </label>
+                <input
+                    className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
+                    id="grid-first-name" type="text" placeholder="Hospital St Justine "/>
+
+            </div>
+
+            <div className="md:flex w-2/5 mb-2 ml-2">
+                <div className="md:w-1/2 px-3 w-96">
+                    <label
+                        className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Type of facility</label>
+                    <div className="relative">
+                        <select name="province"
+                                className="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded">
+                            <option>Clinic</option>
+                            <option>Hospital</option>
+                            <option>Special Installment</option>
+
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="-mx-3 md:flex mb-6">
+            <div className="md:w-1/4 px-3">
+                <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                       htmlFor="grid-last-name">
+                    Phone Number
+                </label>
+                <input
+                    className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
+                    id="grid-last-name" type="text" placeholder="(xxx) xxx - xxxx"/>
+            </div>
+
+            <div className="md:w-1/3 px-3">
+                <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                       htmlFor="grid-last-name">
+                    Website
+                </label>
+                <input
+                    className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
+                    id="grid-last-name" type="text" placeholder="https://"/>
+            </div>
+        </div>
+        <div className="-mx-3 md:flex mb-6">
+            <div className="-mx-3 md:flex ml-1">
+                <div className="md:w-5/6 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Address
+                    </label>
+                    <input name="address" className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" type="text" placeholder="street number street name"/>
+                </div>
+                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">City</label>
+                    <input name="city" className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" type="text" placeholder="Montreal"/>
+                </div>
+                <div className="md:w-1/3 px-3">
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Postal Code</label>
+                    <input name="postal_code" className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" type="text" placeholder="A1A 1A1"/>
+                </div>
+            </div>
+            <div className="md:flex w-2/5 mb-2 ml-2">
+                <div className="md:w-1/2 px-3 w-96">
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Province</label>
+                    <div className="relative">
+                        <select name="province" class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded">
+                            <option>Alberta</option>
+                            <option>British Columbia</option>
+                            <option>Manitoba</option>
+                            <option>New Brunswick</option>
+                            <option>Newfoundland and Labrador</option>
+                            <option>Nova Scotia</option>
+                            <option>Ontario</option>
+                            <option>Prince Edward Island</option>
+                            <option>Quebec</option>
+                            <option>Saskatchewan</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+            <button type="submit" class="bg-transparent hover:bg-grey text-grey-dark font-semibold hover:text-white py-2 px-4 border border-grey hover:border-transparent rounded mr-2"> Submit </button>
+        </div>
+    </div>
+</form>)
+
 }
--- resources/js/components/pages/Worker/AllWorkers.js
+++ resources/js/components/pages/Worker/AllWorkers.js
@@ -11,61 +11,36 @@ export default function () {
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
             <div className="-mx-3 md:flex mb-6">
                 <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-first-name">
-                        First Name
-                    </label>
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">First Name</label>
                     <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
-                        id="grid-first-name" type="text" placeholder="Jane"/>
-
+                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" type="text" placeholder="Jane"/>
                 </div>
                 <div className="md:w-1/2 px-3">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-last-name">
-                        Last Name
-                    </label>
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Last Name</label>
                     <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-last-name" type="text" placeholder="Doe"/>
+                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" type="text" placeholder="Doe"/>
                 </div>
             </div>
             <div className="-mx-3 md:flex mb-2">
                 <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Poste
-                    </label>
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Poste</label>
                     <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="text" placeholder=""/>
+                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" type="text" placeholder=""/>
                 </div>
                 <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Employee ID
-                    </label>
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Employee ID</label>
                     <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="date" placeholder=""/>
+                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" type="text" placeholder=""/>
                 </div>
                 <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-city">
-                        Facility Name
-                    </label>
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Facility Name</label>
                     <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="text" placeholder=""/>
+                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" type="text" placeholder=""/>
                 </div>
                 <div className="md:w-1/2 px-3">
-                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                           htmlFor="grid-zip">
-                        Facility ID
-                    </label>
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Facility ID</label>
                     <input
-                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-zip" type="text" placeholder=""/>
+                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" type="text" placeholder=""/>
                 </div>
             </div>
 
@@ -77,52 +52,38 @@ export default function () {
                 <div className="-mx-3 md:flex mb-2">
                     <div className="grid-cols-2">
                         <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Monday</label>
-                            <input type="time"
-                                   className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
-                            <input type="time"
-                                   className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                            <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                            <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
                     <div className="grid-cols-2">
                         <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Tuesday</label>
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
                     <div className="grid-cols-2">
                         <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Wednesday</label>
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
                     <div className="grid-cols-2">
                         <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Thursday</label>
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
                     <div className="grid-cols-2">
                         <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Friday</label>
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
                     <div className="grid-cols-2">
                         <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Saturday</label>
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
                     <div className="grid-cols-2">
                         <label className="m-2 uppercase  text-grey-darker text-xs pl-10 font-bold">  Sunday</label>
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
-                        <input type="time"
-                               className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" />
+                        <input type="time" className="m-2 appearance-none block w-50 bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"/>
                     </div>
                 </div>
                 </div>
