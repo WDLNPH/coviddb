caf437ea1cecb16a946f03fa450c5bf0b5df8166
Belle-coder <ribelle.el.ayoubi@gmail.com>
2021-04-04 10:04:32 -0400

    group zone form created

--- resources/js/components/pages/GroupZone/AllGroupZones.js
+++ resources/js/components/pages/GroupZone/AllGroupZones.js
@@ -1,5 +1,45 @@
 import React from 'react';
 
-export default function () {
-    return <>Implement list here</>
+export default function ()   {
+
+    function handleSubmit() {
+        alert('A form was submitted ' );
+    }
+    return  (       <form onSubmit={handleSubmit}>
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
+            <div className="-mx-3 md:flex mb-6">
+                <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                           htmlFor="grid-first-name">
+                        Name of Group Zone
+                    </label>
+                    <input
+                        className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
+                        id="grid-first-name" type="text" placeholder="Hospital St Justine "/>
+
+                </div>
+
+                <div className="md:flex w-2/5 mb-2 ml-2">
+                    <div className="md:w-1/2 px-3 w-96">
+                        <label
+                            className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Type of Activity</label>
+                        <div className="relative">
+                            <select name="province"
+                                    className="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded">
+                                <option>Education</option>
+                                <option>House of Worship</option>
+                                <option>Shopping</option>
+                                <option>Sports</option>
+
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+                <button type="submit" class="bg-transparent hover:bg-grey text-grey-dark font-semibold hover:text-white py-2 px-4 border border-grey hover:border-transparent rounded mr-2"> Submit </button>
+            </div>
+        </div>
+    </form>)
+
 }
