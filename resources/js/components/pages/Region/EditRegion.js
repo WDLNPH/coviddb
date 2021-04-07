f779e5495e85edf79dacfce97191b6b84cc98deb
Benjamin Lofo Follo <benjamin.lofo@gmail.com>
2021-04-07 13:18:05 -0400

    Add colors

--- resources/js/components/pages/Region/EditRegion.js
+++ resources/js/components/pages/Region/EditRegion.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {updateFacility} from "../../../api";
-import {Formik} from "formik";
+import {Formik, Field} from "formik";
 import {useParams} from "react-router";
 
 const ALERT_LEVEL_ONE = 1;
@@ -57,6 +57,7 @@ const ALERTS = {
         ]
     },
 }
+
 const ALERT_LEVEL_STRINGS = {
     [ALERT_LEVEL_ONE]: 'Level 1: Vigilance',
     [ALERT_LEVEL_TWO]: 'Level 2: Early Warning',
@@ -72,84 +73,88 @@ export default function () {
         // fetch the patient object from the db
     }, [])
 
-    return loading ? <>please wait</> : <>
+    return loading ? <>please wait</> : (
         <Formik
             initialValues={{
-                alert_level_id: '',
+                alert_level_id: ALERT_LEVEL_ONE,
             }}>
-            <div className="custom-number-input h-10 w-96">
-                <label htmlFor="custom-input-number" className="w-full text-gray-700 text-sm font-semibold">Counter
-                    Input
-                </label>
-                <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-                    <Field name="alert_level_id">
-                        {() => {
-                            function increment() {
-                                if ((alertLevel + 1) <= ALERT_LEVEL_FOUR) {
-                                    setAlertLevel(alertLevel + 1);
-                                }
-                            }
-                            function decrement() {
-                                if ((alertLevel - 1) >= ALERT_LEVEL_ONE) {
-                                    setAlertLevel(alertLevel - 1);
-                                }
-                            }
-                            return (
-                                <>
-                                    <button
-                                        onClick={decrement}
-                                        className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
-                                        <span className="m-auto text-2xl font-thin">−</span>
-                                    </button>
-                                    <input
-                                        onChange={() => {}}
-                                       type="text"
-                                       className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
-                                       name="custom-input-number" value={ALERT_LEVEL_STRINGS[alertLevel]}/>
-                                    <button
-                                        onClick={increment}
-                                        className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
-                                        <span className="m-auto text-2xl font-thin">+</span>
-                                    </button>
-                                </>
-                            );
-                        }}
-                    </Field>
-                </div>
-            </div>
-            <div
-                className="flex flex-col items-center justify-center mt-16 space-y-8 lg:flex-row lg:items-stretch lg:space-x-8 lg:space-y-0"
-            >
-                {[ALERT_LEVEL_ONE,ALERT_LEVEL_TWO,ALERT_LEVEL_THREE,ALERT_LEVEL_FOUR].map(alertCode => (
-                    <section className={`flex flex-col w-full max-w-sm p-12 space-y-6 rounded-lg shadow-md ${ALERTS[alertCode].background}`}>
-                        <div className="flex-shrink-0">
-                            {/*:className="plan.name == 'Basic' ? 'text-green-500' : ''"*/}
-                            <span className="text-4xl font-medium tracking-tight">
-                                {ALERTS[alertCode].header}
-                            </span>
-                            <span className="text-gray-400"></span>
+            {({values}) => (
+                <>
+                    <div className="custom-number-input h-10 w-96">
+                        <label htmlFor="custom-input-number" className="w-full text-gray-700 text-sm font-semibold">Counter
+                            Input
+                        </label>
+                        <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                            <Field name="alert_level_id">
+                                {({field, form: {setFieldValue}}) => {
+                                    function increment() {
+                                        if ((field.value + 1) <= ALERT_LEVEL_FOUR) {
+                                            setFieldValue(field.name, field.value + 1);
+                                        }
+                                    }
+                                    function decrement() {
+                                        if ((field.value - 1) >= ALERT_LEVEL_ONE) {
+                                            setFieldValue(field.name, field.value - 1);
+                                        }
+                                    }
+                                    return (
+                                        <>
+                                            <button
+                                                onClick={decrement}
+                                                className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+                                                <span className="m-auto text-2xl font-thin">−</span>
+                                            </button>
+                                            <input
+                                                onChange={() => {}}
+                                               type="text"
+                                               className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
+                                               name="custom-input-number" value={ALERT_LEVEL_STRINGS[field.value]}/>
+                                            <button
+                                                onClick={increment}
+                                                className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+                                                <span className="m-auto text-2xl font-thin">+</span>
+                                            </button>
+                                        </>
+                                    );
+                                }}
+                            </Field>
                         </div>
-                        <div className="flex-shrink-0 pb-6 space-y-2 border-b">
-                            <h2 className="text-2xl font-normal">{ALERTS[alertCode].description}</h2>
-                            <p className="text-sm text-gray-400">{ALERTS[alertCode].measures}</p>
-                        </div>
+                    </div>
+                    <div
+                        className="flex flex-col  items-center justify-center mt-16 space-y-8 lg:flex-row lg:items-stretch lg:space-x-8 lg:space-y-0"
+                    >
+                        {[ALERT_LEVEL_ONE,ALERT_LEVEL_TWO,ALERT_LEVEL_THREE,ALERT_LEVEL_FOUR].map(alertCode => (
+                            <section className={`mp-alert-section ${ALERTS[alertCode].background} ${values.alert_level_id === alertCode ? 'border-2 border-gray-400' : ''}`}>
+                                <div className="flex-shrink-0">
+                                    {/*:className="plan.name == 'Basic' ? 'text-green-500' : ''"*/}
+                                    <span className="text-4xl font-medium tracking-tight">
+                                        {ALERTS[alertCode].header}
+                                    </span>
+                                    <span className="text-gray-400"></span>
+                                </div>
+                                <div className="flex-shrink-0 pb-6 space-y-2 border-b">
+                                    <h2 className="text-2xl font-normal">{ALERTS[alertCode].description}</h2>
+                                    <p className="text-sm text-gray-400">{ALERTS[alertCode].measures}</p>
+                                </div>
 
-                        <ul className="flex-1 space-y-4">
-                            {ALERTS[alertCode].restrictions.map(restriction =>(
-                                <li className="flex items-start">
-                                    <span className="ml-3 text-base font-medium">- {restriction}</span>
-                                </li>
+                                <ul className="flex-1 space-y-4">
+                                    {ALERTS[alertCode].restrictions.map(restriction =>(
+                                        <li className="flex items-start">
+                                            <span className="ml-3 text-base font-medium">- {restriction}</span>
+                                        </li>
+                                    ))}
+                                </ul>
+
+                                <div className="flex-shrink-0 pt-4">
+                                {/*    <button className="inline-flex items-center justify-center w-full max-w-xs px-4 py-2 transition-colors border rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                        Get Plan
+                                    </button>*/}
+                                </div>
+                        </section>
                             ))}
-                        </ul>
-
-                        <div className="flex-shrink-0 pt-4">
-                        {/*    <button className="inline-flex items-center justify-center w-full max-w-xs px-4 py-2 transition-colors border rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                Get Plan
-                            </button>*/}
-                        </div>
-                </section>
-                    ))}
-            </div>
+                    </div>
+                </>
+            )}
         </Formik>
-    </>
+    );
 }
