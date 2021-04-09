a581aa77e13b9971f1b5b51fcadd6eaaee6bf3e7
Belle-coder <ribelle.el.ayoubi@gmail.com>
2021-04-09 16:18:07 -0400

    radio buttons for diagnostics form

--- resources/js/components/pages/Diagnostics/DiagnosticsForm.js
+++ resources/js/components/pages/Diagnostics/DiagnosticsForm.js
@@ -44,11 +44,12 @@ export default function ({diagnosticsRequestPromise, diagnostics}) {
                                    type="text" placeholder="john"/>
                         </div>
                         <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Last Name</label>
+                            <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Last   Name</label>
                             <Field name="last"
                                    className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
                                    type="text" placeholder="doe"/>
                         </div>
+
 
                     </div>
                     <div className="-mx-3 md:flex mb-6">
@@ -65,6 +66,24 @@ export default function ({diagnosticsRequestPromise, diagnostics}) {
                             </label>
                             <Field name="hcid"
                                    className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" type="text" placeholder="hopital "/>
+                        </div>
+                    </div>
+                    <div className="-mx-3 md:flex mb-6">
+                        <div className="md:w-1/2 px-3">
+                            <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Diagnostic Date </label>
+                            <Field name="date"
+                                   className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"type="date" placeholder="0000"/>
+                        </div>
+
+                        <div className="md:w-1/2 px-3">
+                            <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                                   htmlFor="grid-last-name">
+                                Results
+                            </label>
+                            <Field name="result" id="negative"
+                                   className="m-3 form-radio h-5 w-5 text-red-600"  type="radio" value="negative "/><span className="m-2 text-gray-700">negative</span>
+                            <Field name="result" id="positive"
+                                   className="m-3 form-radio h-5 w-5 text-red-600"  type="radio" value="positive"/><span className="m-2 text-gray-700">positive</span>
                         </div>
                     </div>
                  <div className="md:w-1/2 px-3 mb-6 md:mb-0">
