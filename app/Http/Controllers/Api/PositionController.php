3ce0df2e8143e2c3e4fe90249d1f86350988a2be
Mr Robot <43685289+Makaveliunit@users.noreply.github.com>
2021-04-09 20:23:41 -0400

    Position CRUD done

--- app/Http/Controllers/Api/PositionController.php
+++ app/Http/Controllers/Api/PositionController.php
@@ -10,13 +10,16 @@ class PositionController extends Controller
 {
     /**
      * Create a newly created resource in storage.
-     *
+     *  POST example
+     * {"position":"new position"}
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function create(Request $request)
     {
-        //
+        DB::insert("INSERT INTO Position (`position`) VALUES (?)", [$request->input('position')]);
+        
+        return response()->json("Position Added!");
     }
 
     /**
@@ -31,13 +34,15 @@ class PositionController extends Controller
 
     /**
      * Display the specified resource.
-     *
+     * EXAMPLE GET
+     * 127.0.0.1:8000/api/positions/1
+     * return id = 1
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function readOne($id)
     {
-        //
+        return response()->json(DB::select("SELECT position FROM Position WHERE $id = id"));
     }
 
     /**
@@ -49,7 +54,11 @@ class PositionController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+       
+       
+        $field  = $request->input('field');
+
+        DB::update("UPDATE Position  SET position = (?) WHERE id = $id", [$field]);
     }
 
     /**
@@ -60,6 +69,6 @@ class PositionController extends Controller
      */
     public function delete($id)
     {
-        //
+        DB::delete("DELETE FROM Position WHERE id = $id");
     }
 }
