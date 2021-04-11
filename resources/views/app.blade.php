aea856be8128dbda4cc540e1103b20e3b7ea2010
Benjamin Lofo Follo <benjamin.lofo@gmail.com>
2021-04-10 22:40:19 -0400

    I guess I’ve been working so hard, I forgot what it’s like to be hardly working. - Michael Scott

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="user-token" content="{{ auth()->user()->api_token }}">
-    <link href="{{ asset(app()->environment('production') ? 'coviddb/css/app.css' : 'css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset(app()->environment('production') ? 'coviddb/css/app.css' : 'css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="root">
@@ -14,6 +14,6 @@
 <script>
     window.user = JSON.parse('{!! json_encode(auth()->user())!!}');
 </script>
-<script src="{{mix(app()->environment('production') ? 'coviddb/js/app.js' : 'js/app.js')}}"></script>
+<script src="{{ secure_asset(app()->environment('production') ? 'coviddb/js/app.js' : 'js/app.js')}}"></script>
 </body>
 </html>
