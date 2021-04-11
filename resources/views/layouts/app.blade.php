4603a8db27e506ebd4984b2118201db157cfda3d
Benjamin Lofo Follo <benjamin.lofo@gmail.com>
2021-04-10 22:38:34 -0400

    I'm not superstitious but I am a little stitious. - Michael Scott

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset(app()->environment('production') ? 'coviddb/js/app.js' : 'js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset(app()->environment('production') ? 'coviddb/css/app.css' : 'css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
