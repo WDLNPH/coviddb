5ce9a8aeadda20462b1a2302ef1d917d826d863b
Mr Robot <43685289+Makaveliunit@users.noreply.github.com>
2021-04-15 19:29:51 -0400

    if positive send a reminder done. Diag trigger complete

--- database/sql/triggers.sql
+++ database/sql/triggers.sql
@@ -27,9 +27,10 @@ CREATE TRIGGER diagTrigger
 AFTER INSERT ON Diagnostic
 FOR EACH ROW
 BEGIN
+IF (NEW.result = 1) THEN
 INSERT INTO messages(`message`, `region_id`, `msg_date`,`person_id`, `alert_id`)
 SELECT 
-  CONCAT('Bonsoir ', ps.first_name, '. You have received a diagnostic result. Please log in to letmegraduateCOVIDDB.com to view the result.'),
+  CONCAT('Bonsoir ', ps.first_name, '. You have received a POSITIVE result for your covid diagnostic.'),
   null AS 'region_id',
   NEW.diagnostic_date AS 'msg_date',
   ps.person_id AS 'person_id',
@@ -37,5 +38,29 @@ SELECT
 FROM 
   Person ps
   JOIN patient pc ON pc.person_id = ps.person_id AND NEW.patient_id = pc.patient_id;
+    INSERT INTO messages(`message`, `region_id`, `msg_date`,`person_id`, `alert_id`)
+SELECT 
+  CONCAT('Bonsoir ', ps.first_name, '. You have recently received a POSITIVE diagnostic for COVID-19. Here are the public health recommendations that should be followed during 
+  14 consecutive day after your diagnostic www.coviddb.com/recommendations. Reminder: Do not forget to fill up the follow-up-form on www.coviddb.com using your medicare <', ps.medicare, '> as username and your date of birth <', ps.dob, '> as password.'),
+  null AS 'region_id',
+  NEW.diagnostic_date AS 'msg_date',
+  ps.person_id AS 'person_id',
+  null AS 'alert_id'
+FROM 
+  Person ps
+  JOIN patient pc ON pc.person_id = ps.person_id AND NEW.patient_id = pc.patient_id;
+  END IF;
+IF (NEW.result = 0) THEN
+INSERT INTO messages(`message`, `region_id`, `msg_date`,`person_id`, `alert_id`)
+SELECT 
+  CONCAT('Bonsoir ', ps.first_name, '. You have received a NEGATIVE result for your covid diagnostic.'),
+  null AS 'region_id',
+  NEW.diagnostic_date AS 'msg_date',
+  ps.person_id AS 'person_id',
+  null AS 'alert_id'
+FROM 
+  Person ps
+  JOIN patient pc ON pc.person_id = ps.person_id AND NEW.patient_id = pc.patient_id;
+  END IF;
 END; //
 DELIMITER ;
