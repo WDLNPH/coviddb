8af8f7562fb8cace4cb03237166d6b51065ef225
Belle-coder <ribelle.el.ayoubi@gmail.com>
2021-04-16 12:55:52 -0400

    api updates

---
+++ resources/js/components/pages/MessageHistory/AllMessages.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useMemo, useState} from 'react';
+import {NavLink, Route, Switch} from "react-router-dom";
+import CreateRecommendations from "./CreateRecommendations";
+import EditRecommendations from "./EditRecommendations";
+import {useHistory, useRouteMatch} from "react-router";
+import Table from "../../Table";
+import {readAllRecommendations} from "../../../api";
+
+const RECOMMENDATIONS_COLUMNS = ['recommendation'];
+
+export default function () {
+    //Recommendations
+    const match = useRouteMatch();
+    return  (
+        <>
+            <Switch>
+                <Route path={`${match.url}/create`} component={CreateRecommendations}/>
+                <Route path={`${match.url}/:recommendationId`} component={EditRecommendations}/>
+                <Route render={() => (
+                    <>
+                        <div className="mp-page-header">
+                            <h1 className="mp-page-header-title">List of All Recommendations</h1>
+                            <NavLink to={`${match.url}/create`} className="mp-button w-max">Create a new Recommendations</NavLink>
+                        </div>
+                        <ListRecommendations/>
+                    </>
+                )}/>
+            </Switch>
+        </>
+    )
+}
+
+function ListRecommendations() {
+    const [recommendations, setRecommendations] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const history = useHistory()
+
+    // componentDidMount
+    useEffect(() => {
+        async function loadRecommendations() {
+            setLoading(true);
+            try {
+                const {data} = await readAllRecommendations();
+                console.log(data);
+                setRecommendations(data);
+            } catch (e) {
+                // skip
+            }
+            setLoading(false);
+        }
+        loadRecommendations()
+    }, []);
+
+    const memoizedColumns = useMemo(() => RECOMMENDATIONS_COLUMNS.map(col => ({
+        Header: col,
+        accessor: col
+    })), []);
+
+    return loading ? '...' : <Table onClick={(recommendation) =>  history.push(`/recommendations/${recommendation.recommendation_id}`)} columns={memoizedColumns} data={recommendations}/>;
+}
